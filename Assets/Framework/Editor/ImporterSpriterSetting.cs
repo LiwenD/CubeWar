43a3d8542370b3596c243d05e9ac9615e1d3559b
LiwenD <dengliwen1997@outlook.com>
2019-06-11 21:53:20 +0800

    no message

---
+++ Assets/Framework/Editor/ImporterSpriterSetting.cs
@@ -0,0 +1,116 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEditor;
+
+public class ImporterSpriterSetting : EditorWindow
+{
+
+    [MenuItem("Yummy/快速设置精灵")]
+    static void ImporterSpriterSettingWindow()
+    {
+        //创建窗口
+        Rect wr = new Rect(500, 500, 500, 500);
+        ImporterSpriterSetting window = (ImporterSpriterSetting)EditorWindow.GetWindowWithRect(typeof(ImporterSpriterSetting), wr, false, "ImporterSpriterSetting");
+        window.Show();
+    }
+
+    TextureImporterType textureType = TextureImporterType.Sprite;
+    SpriteImportMode spriteMode = SpriteImportMode.Single;
+    string packingTag = string.Empty;
+    bool sRGBTexture = true;
+    bool alphaIsTransparency = true;
+    bool mipmapEnabled = false;
+    TextureWrapMode wrapMode = TextureWrapMode.Clamp;
+    FilterMode filterMode = FilterMode.Bilinear;
+
+    TextureImporterFormat textureImporterFormatAndroid = TextureImporterFormat.ETC2_RGBA8;
+    TextureImporterFormat textureImporterFormatiPhone = TextureImporterFormat.PVRTC_RGBA4;
+    TextureImporterFormat textureImporterFormatStandalone = TextureImporterFormat.DXT5;
+
+    void OnGUI()
+    {
+
+        textureType = (TextureImporterType)EditorGUILayout.EnumPopup("Texture Type", textureType);
+        EditorGUILayout.Space();//空一行
+
+        spriteMode = (SpriteImportMode)EditorGUILayout.EnumPopup("Sprite Mode", spriteMode);
+        EditorGUILayout.Space();//空一行
+
+        packingTag = EditorGUILayout.TextField("Packing Tag", packingTag);
+        EditorGUILayout.Space();//空一行
+
+        sRGBTexture = EditorGUILayout.Toggle("S RGB Texture", sRGBTexture);
+        EditorGUILayout.Space();//空一行
+
+        alphaIsTransparency = EditorGUILayout.Toggle("Alpha Is Transparency", alphaIsTransparency);
+        EditorGUILayout.Space();//空一行
+
+        mipmapEnabled = EditorGUILayout.Toggle("Generate Mip Map", mipmapEnabled);
+        EditorGUILayout.Space();//空一行
+
+        wrapMode = (TextureWrapMode)EditorGUILayout.EnumPopup("Wrap Mode", wrapMode);
+        EditorGUILayout.Space();//空一行
+
+        filterMode = (FilterMode)EditorGUILayout.EnumPopup("Filter Mode", filterMode);
+        EditorGUILayout.Space();//空一行
+
+        textureImporterFormatAndroid = (TextureImporterFormat)EditorGUILayout.EnumPopup("Texture Importer Format For Android", textureImporterFormatAndroid);
+        EditorGUILayout.Space();//空一行
+
+        textureImporterFormatiPhone = (TextureImporterFormat)EditorGUILayout.EnumPopup("Texture Importer Format For iPhone", textureImporterFormatiPhone);
+        EditorGUILayout.Space();//空一行
+
+        textureImporterFormatStandalone = (TextureImporterFormat)EditorGUILayout.EnumPopup("Texture Importer Format For Standalone", textureImporterFormatStandalone);
+        EditorGUILayout.Space();//空一行
+
+
+
+        if (GUILayout.Button("设置Sprite", GUILayout.ExpandWidth(true)))
+        {
+            Object[] targetObj = Selection.GetFiltered(typeof(UnityEngine.Object), SelectionMode.DeepAssets);
+            if (targetObj != null && targetObj.Length > 0)
+            {
+                for (int i = 0; i < targetObj.Length; i++)
+                {
+                    if (targetObj[i] is Texture)
+                    {
+                        string path = AssetDatabase.GetAssetPath(targetObj[i]);
+                        TextureImporter texture = AssetImporter.GetAtPath(path) as TextureImporter;
+                        texture.textureType = textureType;
+                        texture.spriteImportMode = spriteMode;
+                        texture.spritePixelsPerUnit = 100;
+                        texture.sRGBTexture = sRGBTexture;
+                        texture.alphaIsTransparency = alphaIsTransparency;
+                        texture.filterMode = filterMode;
+                        texture.mipmapEnabled = mipmapEnabled;
+                        texture.wrapMode = wrapMode;
+
+                        TextureImporterPlatformSettings platformSetting = new TextureImporterPlatformSettings();
+                        platformSetting.name = "Android";
+                        platformSetting.format = textureImporterFormatAndroid;
+                        platformSetting.overridden = true;
+                        platformSetting.maxTextureSize = 2048;
+                        texture.SetPlatformTextureSettings(platformSetting);
+
+                        TextureImporterPlatformSettings platformSettingIP = new TextureImporterPlatformSettings();
+                        platformSettingIP.name = "iPhone";
+                        platformSettingIP.format = textureImporterFormatiPhone;
+                        platformSettingIP.overridden = true;
+                        platformSettingIP.maxTextureSize = 2048;
+                        texture.SetPlatformTextureSettings(platformSettingIP);
+
+                        TextureImporterPlatformSettings platformSettingStandalone = new TextureImporterPlatformSettings();
+                        platformSettingStandalone.name = "Standalone";
+                        platformSettingStandalone.format = textureImporterFormatStandalone;
+                        platformSettingStandalone.overridden = true;
+                        platformSettingStandalone.maxTextureSize = 2048;
+                        texture.SetPlatformTextureSettings(platformSettingStandalone);
+
+                        AssetDatabase.ImportAsset(path);
+                    }
+                }
+            }
+        }
+    }
+}
--- Assets/Framework/Event/EventTest.cs
+++ Assets/Framework/Event/EventTest.cs
@@ -1,67 +1,67 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using UnityEngine;
-using YummyGame.Framework;
+﻿//using System;
+//using System.Collections.Generic;
+//using System.Linq;
+//using System.Text;
+//using UnityEngine;
+//using YummyGame.Framework;
 
-//消息定义类,其中的泛型参数代表消息传递时候的参数类型
-//每种消息一个对应的实例，并且设成静态对象
-class Msg
-{
-    //一个参数
-    public static readonly MsgWrapper<EventArgs> Event1 = new MsgWrapper<EventArgs>();
-    //两个参数
-    public static readonly MsgWrapper<int,Vector3> Event2 = new MsgWrapper<int, Vector3>();
-    //没有参数
-    public static readonly MsgWrapper Event3 = new MsgWrapper();
-}
+////消息定义类,其中的泛型参数代表消息传递时候的参数类型
+////每种消息一个对应的实例，并且设成静态对象
+//class Msg
+//{
+//    //一个参数
+//    public static readonly MsgWrapper<EventArgs> Event1 = new MsgWrapper<EventArgs>();
+//    //两个参数
+//    public static readonly MsgWrapper<int,Vector3> Event2 = new MsgWrapper<int, Vector3>();
+//    //没有参数
+//    public static readonly MsgWrapper Event3 = new MsgWrapper();
+//}
 
-class EventArgs
-{
-    public int id;
-    public string name;
-}
+//class EventArgs
+//{
+//    public int id;
+//    public string name;
+//}
 
 
-internal class EventTest:MonoBehaviour
-{
-    private void Start()
-    {
-        //监听
-        Msg.Event1.AddListener(OnEvent1);
-        Msg.Event2.AddListener(OnEvent2);
+//internal class EventTest:MonoBehaviour
+//{
+//    private void Start()
+//    {
+//        //监听
+//        Msg.Event1.AddListener(OnEvent1);
+//        Msg.Event2.AddListener(OnEvent2);
 
-    }
+//    }
 
-    private void OnEvent2(int args1,Vector3 args2)
-    {
-        Debug.Log(args1);
-        Debug.Log(args2);
-    }
+//    private void OnEvent2(int args1,Vector3 args2)
+//    {
+//        Debug.Log(args1);
+//        Debug.Log(args2);
+//    }
 
-    private void OnEvent1(EventArgs obj)
-    {
-        Debug.Log(obj.id);
-        Debug.Log(obj.name);
-    }
+//    private void OnEvent1(EventArgs obj)
+//    {
+//        Debug.Log(obj.id);
+//        Debug.Log(obj.name);
+//    }
 
-    private void Update()
-    {
-        //发送事件
-        if (Input.GetKeyDown(KeyCode.Q))
-        {
-            Msg.Event1.Dispach(new EventArgs() { id = 1, name = "222" });
-        }
+//    private void Update()
+//    {
+//        //发送事件
+//        if (Input.GetKeyDown(KeyCode.Q))
+//        {
+//            Msg.Event1.Dispach(new EventArgs() { id = 1, name = "222" });
+//        }
 
-        if (Input.GetKeyDown(KeyCode.W))
-        {
-            Msg.Event2.Dispach(1,Vector3.zero);
-        }
+//        if (Input.GetKeyDown(KeyCode.W))
+//        {
+//            Msg.Event2.Dispach(1,Vector3.zero);
+//        }
 
-        if (Input.GetKeyDown(KeyCode.C))
-        {
-            Msg.Event1.RemoveListener(OnEvent1);
-        }
-    }
-}
+//        if (Input.GetKeyDown(KeyCode.C))
+//        {
+//            Msg.Event1.RemoveListener(OnEvent1);
+//        }
+//    }
+//}
