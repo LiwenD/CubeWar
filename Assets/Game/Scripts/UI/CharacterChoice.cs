270febacea07e6ce5715ca5a3105b7518e97c97b
LiwenD <dengliwen1997@outlook.com>
2019-06-16 10:23:28 +0800

    no message

---
+++ Assets/Game/Scripts/UI/CharacterChoice.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+using YummyGame.Framework;
+
+namespace YummyGame.CubeWar
+{
+
+    public class CharacterChoice : UIWindow
+    {
+        public override string ResourcePath => "UI/CharacterChoice";
+
+        [UIClick("BtnUp")]
+        [Inst("BtnUp")]
+        public Button btnUp;
+
+        [UIClick("BtnDown")]
+        [Inst("BtnDown")]
+        public Button btnDown;
+
+        [UIClick("BtnEnter")]
+        [Inst("BtnEnter")]
+        public Button btnEnter;
+
+
+        void BtnUp()
+        {
+            Debug.Log("BtnUp");
+        }
+
+        void BtnDown()
+        {
+            Debug.Log("BtnDown");
+        }
+
+        void BtnEnter()
+        {
+            Debug.Log("BtnEnter");
+        }
+    }
+}
